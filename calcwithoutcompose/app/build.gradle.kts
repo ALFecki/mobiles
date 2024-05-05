976d7fd045306014acacdbef19bc30ea6c1f52fc
alfecki <alexskvr03@gmail.com>
2024-05-05 18:46:14 +0300

    added database changes

--- calcwithoutcompose/app/build.gradle.kts
+++ calcwithoutcompose/app/build.gradle.kts
@@ -43,8 +43,13 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation("androidx.core:core-ktx:+")
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.android.gms:play-services-auth:21.1.0")
+
+
 }
